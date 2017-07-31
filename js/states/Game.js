b819467b159d41b5955aeddbd2958b593937e162
lischiang <lisa.chiang.sv@gmail.com>
2017-07-30 22:08:44 -0400

    Added cactus

--- js/states/Game.js
+++ js/states/Game.js
@@ -12,6 +12,7 @@
     p.counter = 0;
     p.msgTxt = null;
     p.linesContainer = null;
+    p.environmentContainer = null;
     //p.character = null;
     //p.character.nextX = 0;
 
@@ -22,7 +23,8 @@
         this.addRoad();
         this.createLinesContainer();
         this.addLines();
-
+        this.createEnvironmentContainer();
+        this.addTrees();
 
     }
     p.addBG = function () {
@@ -69,7 +71,10 @@
         this.addChild(this.linesContainer);
 
     }
-
+    p.createEnvironmentContainer = function () {
+        this.environmentContainer = new createjs.Container();
+        this.addChild(this.environmentContainer);
+    }
     p.addRoad = function () {
         var road = new createjs.Shape();
         road.graphics.beginFill('#DDC').drawRect(0, 0, 400, 600);
@@ -77,7 +82,48 @@
         road.y = 0;
         this.addChild(road);
     }
+    p.addTrees = function () {
+        var tree;
+        var trees = this.environmentContainer;
+        tree = new Cactus();
+        tree.x = 100;
+        tree.y = 0;
+        tree.speed = 5;
+        trees.addChild(tree);
 
+        var tree;
+        var trees = this.environmentContainer;
+        tree = new Cactus();
+        tree.x = 700;
+        tree.y = 0;
+        tree.speed = 5;
+        trees.addChild(tree);
+    }
+    p.addingNewTrees = function () {
+        var treeDiv = 100;
+        var tree;
+        var trees = this.environmentContainer;
+        var len = this.environmentContainer.getNumChildren();
+        tree = trees.getChildAt(len - 1);
+        if (tree.y >= 500 + treeDiv) {
+            this.addTrees();
+
+        }
+    }
+
+    p.removeOldTrees = function () {
+        var tree;
+        var trees = this.environmentContainer;
+        var len = this.environmentContainer.getNumChildren();
+        for (var i = 0; i < len ; i++) {
+            tree = trees.getChildAt(i);
+            if (tree != null) {
+                if (tree.y >= 1200) {
+                    this.environmentContainer.removeChildAt(i);
+                }
+            }
+        }
+    }
     p.addLines = function () {
         var line;
         var lines = this.linesContainer;
@@ -98,9 +144,6 @@
             this.addLines();
 
         }
-
-
-
     }
 
     p.removeOldLines = function () {
@@ -113,19 +156,14 @@
                 if(line.y >= 600) {
                     this.linesContainer.removeChildAt(i);
                 }
-
             }
-
-
-
         }
-
     }
 
 
     p.update = function () {
 
-        var line, nextY, len;
+        var line, nextY, len, tree;
         len = this.linesContainer.getNumChildren();
         for(var i = 0; i< len ; i++){
             line = this.linesContainer.getChildAt(i);
@@ -133,6 +171,13 @@
             line.nextY = nextY;
 
         }
+        len = this.environmentContainer.getNumChildren();
+        for (var i = 0; i < len ; i++) {
+            tree = this.environmentContainer.getChildAt(i);
+            nextY = tree.y + tree.speed;
+            tree.nextY = nextY;
+        }
+
         var nextX;
 
         if (this.leftKeyDown) {
@@ -163,7 +208,13 @@
         }
         //this.character.x = this.character.nextX;
 
+        var tree;
+        len = this.environmentContainer.getNumChildren();
+        for (var i = 0; i < len ; i++) {
+            tree = this.environmentContainer.getChildAt(i);
+            tree.y = tree.nextY;
 
+        }
     }
     p.checkGame = function () {
 
@@ -173,6 +224,8 @@
         this.render();
         this.addingNewLines();
         this.removeOldLines();
+        this.addingNewTrees();
+        this.removeOldTrees();
         window.onkeydown = this.movePlayer;
         window.onkeyup = this.stopPlayer;
 
